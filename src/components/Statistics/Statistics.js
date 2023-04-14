c5be1c7f92fd29dc502955883647b267433a9f0f
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-04-14 22:20:44 +0300

    Create Statistics.js

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,17 @@
+export const Statistics = ({ data, title }) => {
+  console.log(data);
+  return (
+    <section class="statistics">
+      <h2 class="title">{title}</h2>
+
+      <ul class="stat-list">
+        {data.map(({ id, label, percentage }) => (
+          <li key={id} class="item">
+            <span class="label">{label}</span>
+            <span class="percentage">{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
