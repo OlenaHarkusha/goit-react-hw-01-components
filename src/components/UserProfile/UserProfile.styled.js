c9818a43253351ecc305ead78053944f7a1361a6
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-04-14 22:20:56 +0300

    Create UserProfile.styled.js

---
+++ src/components/UserProfile/UserProfile.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Profile = styled.div``;
+
+export const ProfileDescription = styled.div``;
+
+export const DescriptionInfo = styled.p``;
+
+export const DesctiptionName = styled.p``;
+
+export const Stats = styled.ul``;
+
+export const StatsLabel = styled.span``;
+
+export const StatsQuantity = styled.span``;
