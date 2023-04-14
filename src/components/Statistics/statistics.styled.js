102d131eb61f8928e70fa63c3851abc49ccc045a
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-04-14 22:20:46 +0300

    Create statistics.styled.js

---
+++ src/components/Statistics/statistics.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StatsBody = styled.div``;
+
+export const StatsTitle = styled.h2``;
+
+export const StatList = styled.ul``;
+
+export const StatItem = styled.li``;
+
+export const StatLabel = styled.span``;
+
+export const StatPercentage = styled.span``;
